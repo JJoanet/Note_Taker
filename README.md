8e5d423d0520428030f27c0f2f6ca642dcd69e14
JeremyJ <JeremyJoanet@protonmail.com>
2021-04-29 20:59:07 -0500

    init commit, completed project

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Note Taker
+
+# Contact
+Vygoth
+JeremyJoanet@protonmail.com
+
+## Description
+This application allows you to create notes that are stored to a database, you can view or delete those notes after they are created.
+
+## Table of Contents
+- [Usage](#Usage)
+- [Installation](#Dependancies)
+- [Testing](#Testing)
+- [Credits](#Credits)
+- [License](#License)
+- [Contributing](#Contributing)
+- [License Information](#LicenseInfo)
+- [Screenshot](#Screenshot)
+
+## Usage
+To use this app, in your terminal targeted at this file source, type node server.js, then open your browser to localhost:3000, if opening on heroku this process is not needed. In the note section of the app, type your title and text body and click save in the top right corner of the screen. To view a previously created note, click on the title of the saved note you wish to view on the left hand column. To create a new note, click the pencil icon on the top right corner.
+
+## Dependancies
+npm Install
+
+## Testing
+N/A
+
+## Credits
+Matt Reisdorf, Tom Siemion, Zach Duty. 
+
+## Contributing
+N/A
+
+## Screenshot
+![Screenshot](./assets/Screenshot.png)
---
+++ server/server.js
@@ -0,0 +1,87 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const db = require('../db/db.json');
+let idCounter = 1;
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Sets up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('../public'));
+app.use(express.json());
+// Check for Server Running
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
+// Serve HTML Pages
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+// GET Notes Api
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '../db/db.json')));
+// GET Specific Note
+app.get('/api/notes/:note', (req, res) => {
+    // chosen note 
+    const chosen = req.params.note;
+    // for loop iteration, if selected note title is in db.json, response is json of that specific db entry
+    for (let i = 0; i < db.length; i++) {
+        if (chosen === db[i].title) {
+          return res.json(db[i]);
+        };
+      };
+});
+// POST
+app.post('/api/notes', (req, res) => {
+    // New API note
+    const newNote = req.body;
+    // get data from db.json
+    let dbData = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    // parse dbData
+    let parsedDbData = JSON.parse(dbData);
+    // Push note to db.json with id number 
+    newNote.id = verifyID(parsedDbData);
+    console.log(newNote.id, 'new note id');
+    parsedDbData.push(newNote);
+    // Overwrite existing db.json
+    fs.writeFile('../db/db.json', JSON.stringify(parsedDbData), err => {
+        err ? console.error(err) : console.log('Success');
+    });
+    // Reload the page on new note creation
+    res.end();
+});
+// DELETE
+app.delete('/api/notes/:id', (req, res) => {
+
+    const returnId = req.params.id;
+    console.log(returnId);
+    // get data from db.json
+    let dbData = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    // parse dbData
+    let parsedDbData = JSON.parse(dbData);
+    for (let i = 0; i < parsedDbData.length; i++) {
+        if (parsedDbData[i].id == returnId) {
+            console.log()
+            parsedDbData.splice(i, 1);
+        };
+    };
+    // Overwrite existing db.json
+    fs.writeFile('../db/db.json', JSON.stringify(parsedDbData), err => {
+        err ? console.error(err) : console.log('Success');
+    });
+    res.end();
+});
+
+function verifyID(parsedData) {
+    let x = parsedData.length;
+    if (x === 0) {
+        x = 1
+    }
+    else {
+        for (i = 0; i < parsedData.length; i++) {
+            console.log(parsedData[i].id, 'within the function');
+            if (parsedData[i].id === x){
+                x++;
+            }
+        }
+    }
+    return x
+}
